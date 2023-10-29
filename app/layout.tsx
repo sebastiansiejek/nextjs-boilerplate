f2f1690a656f11ee3aa999a89cd8ad1836b187b0
Sebastian Siejek <s.siejek@timecamp.com>
2023-10-29 09:31:35 +0100

    style: change relative import with absolute

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 
-import './styles/global.css'
+import 'styles/global.css'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
